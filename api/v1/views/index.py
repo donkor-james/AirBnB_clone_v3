0b12a4aa3b6d7ed6a7c961246a84e6ff7db6231a
Donkor James <jamesdonkor987@gmail.com>
2024-05-28 19:56:45 +0000

    updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,25 @@
 """ Create FLask app app_view. """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
-@app_views.route('/status')
+@app_views('/status')
 def api_status():
-    """ Response with the status of an api request """
+    """ Response with the status of an api request."""
     response = {'status': 'OK'}
     return jsonify(response)
+
+
+@app_views.route('/stats')
+def get_stats():
+    """stats of models. """
+    stats = {
+        'amenities': storage.count('Amenty'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats)
